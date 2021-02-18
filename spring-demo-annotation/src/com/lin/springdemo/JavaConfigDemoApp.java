1d2a04cb2408a93e1a71ae307f9ee92204b71789
Chen Yu Lin <qwe111845@gmail.com>
2021-02-18 22:15:53 +0800

    feat:Define Spring bean with Java code
    use @Configuration and @ComponentScan define Spring bean

---
+++ spring-demo-annotation/src/com/lin/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,26 @@
+package com.lin.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(SportConfig.class);
+				
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyFortune());
+
+		((TennisCoach) theCoach).getCoach();
+
+		
+		context.close();
+		
+	}
+
+}
---
+++ spring-demo-annotation/src/com/lin/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.lin.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.lin.springdemo")
+public class SportConfig {
+
+}
