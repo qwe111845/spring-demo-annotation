31d6a62ef437cd70e9b8d37d17be088e73921cbd
Chen Yu Lin <qwe111845@gmail.com>
2021-02-18 23:15:28 +0800

    feat: add commit of @ComponentScan

--- spring-demo-annotation/src/com/lin/springdemo/SportConfig.java
+++ spring-demo-annotation/src/com/lin/springdemo/SportConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+//@ComponentScan("com.lin.springdemo")
 @PropertySource("classpath:coach.properties")
 public class SportConfig {
 	
